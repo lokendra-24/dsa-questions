0b52d2f51c5d3ee16934cbc2388990e621f3619b
Lokendra Prajapati <nileshnp7248@gmail.com>
2025-07-30 16:35:54 +0530

    Time: 3 ms (13.06%) | Memory: 17.7 MB (53.45%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
+        
         count_0=0
         count_1=0
         count_2=0
